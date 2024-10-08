2e3ea505f9919e4023b0db30505e2cf52ba867e9
Anders Westberg <anders.westberg@diginet.se>
2024-10-08 09:35:37 +0200

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     open: true,
+    https: {
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem'),
+    },
   },
   sourcemap: true,
   alias: {
